1cc2e7e8d31e8f1f1357473bbdb841b1b796b6d9
PaildarBasanthi <basanthisrec@gmail.com>
2023-07-22 12:04:36 +0530

    first Commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppTask_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int a=int.Parse(Console.ReadLine());
+            if(a%2==0 )
+            {
+                Console.WriteLine("Entered Number is Even");
+            }
+            else
+            {
+                Console.WriteLine("Entered number is Odd");
+            }
+        }
+    }
+}
